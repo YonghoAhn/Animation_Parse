6ee322211d2eb76a3343bbec1b7413cf016ec8e2
YonghoAhn <hrh1110@naver.com>
2016-11-30 11:47:39 +0900

    Data Type Edit

--- Animation_Parse/DataSender.cs
+++ Animation_Parse/DataSender.cs
@@ -9,13 +9,16 @@ namespace Animation_Parse
     class DataSender
     {
         public string Full_HTML { get; set; }
-        public static List<Tuple<string, string[], string>> Mon { get; set; }
-        public static List<Tuple<string, string[], string>> Tue { get; set; }
-        public static List<Tuple<string, string[], string>> Wed { get; set; }
-        public static List<Tuple<string, string[], string>> Thu { get; set; }
-        public static List<Tuple<string, string[], string>> Fri { get; set; }
-        public static List<Tuple<string, string[], string>> Sat { get; set; }
-        public static List<Tuple<string, string[], string>> Sun { get; set; }
-
+        /*
+         * List<Tuple<string,string[],string>> Mon {get;set;}
+         * Mon<Tuple<이름,Group(회차목록|img src)(영상 주소)>> 하면 될듯
+         */
+        public static List<Tuple<string, string[][]>> Mon { get; set; }
+        public static List<Tuple<string, string[][]>> Tue { get; set; }
+        public static List<Tuple<string, string[][]>> Wed { get; set; }
+        public static List<Tuple<string, string[][]>> Thu { get; set; }
+        public static List<Tuple<string, string[][]>> Fri { get; set; }
+        public static List<Tuple<string, string[][]>> Sat { get; set; }
+        public static List<Tuple<string, string[][]>> Sun { get; set; }
     }
 }
