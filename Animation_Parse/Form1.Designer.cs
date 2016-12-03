5b3c95e652fc416c80de6794f093c7f3d8b4aa18
YonghoAhn <hrh1110@naver.com>
2016-12-03 11:59:45 +0900

    Phase 1

--- Animation_Parse/Form1.Designer.cs
+++ Animation_Parse/Form1.Designer.cs
@@ -29,51 +29,19 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.listView2 = new System.Windows.Forms.ListView();
             this.img_Title = new System.Windows.Forms.ImageList(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.Panel1.SuspendLayout();
-            this.splitContainer1.Panel2.SuspendLayout();
-            this.splitContainer1.SuspendLayout();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일FToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.새로고침RToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.최근본애니ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.보기VToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.최근본영상RToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.완결애니CToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.방영애니BToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // splitContainer1
-            // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Name = "splitContainer1";
-            // 
-            // splitContainer1.Panel1
-            // 
-            this.splitContainer1.Panel1.Controls.Add(this.listView1);
-            // 
-            // splitContainer1.Panel2
-            // 
-            this.splitContainer1.Panel2.Controls.Add(this.listView2);
-            this.splitContainer1.Size = new System.Drawing.Size(897, 555);
-            this.splitContainer1.SplitterDistance = 221;
-            this.splitContainer1.TabIndex = 0;
-            // 
-            // listView1
-            // 
-            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView1.Location = new System.Drawing.Point(0, 0);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(221, 555);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.List;
-            // 
-            // listView2
-            // 
-            this.listView2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView2.Location = new System.Drawing.Point(0, 0);
-            this.listView2.Name = "listView2";
-            this.listView2.Size = new System.Drawing.Size(672, 555);
-            this.listView2.TabIndex = 0;
-            this.listView2.UseCompatibleStateImageBehavior = false;
             // 
             // img_Title
             // 
@@ -81,29 +49,117 @@
             this.img_Title.ImageSize = new System.Drawing.Size(16, 16);
             this.img_Title.TransparentColor = System.Drawing.Color.Transparent;
             // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 24);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(897, 531);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일FToolStripMenuItem,
+            this.보기VToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(897, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일FToolStripMenuItem
+            // 
+            this.파일FToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.새로고침RToolStripMenuItem,
+            this.최근본애니ToolStripMenuItem});
+            this.파일FToolStripMenuItem.Name = "파일FToolStripMenuItem";
+            this.파일FToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.파일FToolStripMenuItem.Text = "파일(&F)";
+            // 
+            // 새로고침RToolStripMenuItem
+            // 
+            this.새로고침RToolStripMenuItem.Name = "새로고침RToolStripMenuItem";
+            this.새로고침RToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.새로고침RToolStripMenuItem.Text = "새로 고침(&R)";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(877, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(20, 20);
+            this.webBrowser1.TabIndex = 2;
+            this.webBrowser1.Visible = false;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // 최근본애니ToolStripMenuItem
+            // 
+            this.최근본애니ToolStripMenuItem.Name = "최근본애니ToolStripMenuItem";
+            this.최근본애니ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.최근본애니ToolStripMenuItem.Text = "최근 본 애니";
+            // 
+            // 보기VToolStripMenuItem
+            // 
+            this.보기VToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.최근본영상RToolStripMenuItem,
+            this.완결애니CToolStripMenuItem,
+            this.방영애니BToolStripMenuItem});
+            this.보기VToolStripMenuItem.Name = "보기VToolStripMenuItem";
+            this.보기VToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.보기VToolStripMenuItem.Text = "보기(&V)";
+            // 
+            // 최근본영상RToolStripMenuItem
+            // 
+            this.최근본영상RToolStripMenuItem.Name = "최근본영상RToolStripMenuItem";
+            this.최근본영상RToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.최근본영상RToolStripMenuItem.Text = "최근 본 영상(&R)";
+            // 
+            // 완결애니CToolStripMenuItem
+            // 
+            this.완결애니CToolStripMenuItem.Name = "완결애니CToolStripMenuItem";
+            this.완결애니CToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.완결애니CToolStripMenuItem.Text = "완결 애니(&C)";
+            // 
+            // 방영애니BToolStripMenuItem
+            // 
+            this.방영애니BToolStripMenuItem.Name = "방영애니BToolStripMenuItem";
+            this.방영애니BToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.방영애니BToolStripMenuItem.Text = "방영 애니(&B)";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(897, 555);
-            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            this.splitContainer1.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ListView listView2;
         private System.Windows.Forms.ImageList img_Title;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일FToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 새로고침RToolStripMenuItem;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ToolStripMenuItem 최근본애니ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 보기VToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 최근본영상RToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 완결애니CToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 방영애니BToolStripMenuItem;
     }
 }
 
--- Animation_Parse/Form1.cs
+++ Animation_Parse/Form1.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,10 +16,17 @@ namespace Animation_Parse
 {
     public partial class Form1 : Form
     {
+        private int dayCount = 0;
+        private int pageCount = 0;
+        private ImageList imgList;
+        private string[] srcList;
+        //HTMLParser HTMLParser;
         public Form1()
         {
             InitializeComponent();
-            Task.Factory.StartNew(() => GetAni("http://ani.today/"));
+            webBrowser1.Navigate("http://ani.today/");
+            //Task.Factory.StartNew(() => GetAni("http://ani.today/"));
+            //HtmlElementCollection imgs;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -49,7 +57,56 @@ namespace Animation_Parse
                 response.Close();
                 readStream.Close();
             }
-            MessageBox.Show(data);
+            //HTMLParser.Test(data);
+        }
+
+        private Image LoadImage(string url)
+        {
+            System.Net.WebRequest request =
+                System.Net.WebRequest.Create(url);
+
+            System.Net.WebResponse response = request.GetResponse();
+            System.IO.Stream responseStream =
+                response.GetResponseStream();
+
+            Bitmap bmp = new Bitmap(responseStream);
+
+            responseStream.Dispose();
+
+            return bmp;
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (pageCount == 0)
+            {
+                Console.WriteLine("Run");
+                HtmlDocument docs = webBrowser1.Document;
+                HtmlElementCollection tables = default(HtmlElementCollection);
+                tables = docs.GetElementsByTagName("div");
+                foreach (HtmlElement he in tables)
+                {
+                    if (he.GetAttribute("className").ToString() == "enter-list-entity")
+                    {
+                        if (he.InnerHtml != null)
+                        {
+                            //Console.WriteLine(he.InnerHtml.ToString());
+                            string imgSrc = Regex.Match(he.InnerHtml.ToString(), "src=[\"'](.+?)[\"'].+?", RegexOptions.IgnoreCase).Groups[1].Value;
+                            string titleStr = Regex.Match(he.InnerHtml.ToString(), "title=[\"'](.+?)[\"'].+?", RegexOptions.IgnoreCase).Groups[1].Value;
+                            string hrefStr = Regex.Match(he.InnerHtml.ToString(), "href=[\"'](.+?)[\"'].+?", RegexOptions.IgnoreCase).Groups[1].Value;
+                            Console.WriteLine(titleStr);
+                            Console.WriteLine(imgSrc);
+                            Console.WriteLine(hrefStr);
+                        }
+                        //Thread.Sleep(100);
+                    }
+                    else if(he.GetAttribute("className").ToString() == "enter-list-head")
+                    {
+                        dayCount++;
+                    }
+                }
+                pageCount = 1;
+            }
         }
     }
 }
